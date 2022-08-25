04af99070b33ed6acefb03419844b99973ee421f
Sunny Kim <starwin@gmail.com>
2022-08-26 02:42:46 +0900

    level 1 coins

---
+++ Assets/Scripts/Editor/UnpackCoins.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class UnpackCoins
+{
+    [MenuItem("GameObject/Unpack Coins", false, 101)]
+    public static void UnpackSelectedCoins() {
+        //if it is a prefab unpack it
+        GameObject go = Selection.activeGameObject;
+        if(PrefabUtility.GetPrefabInstanceStatus(go) != PrefabInstanceStatus.NotAPrefab) {
+            PrefabUtility.UnpackPrefabInstance(go, PrefabUnpackMode.OutermostRoot, InteractionMode.AutomatedAction);
+        }
+
+        //move children
+        Transform parent = go.transform.parent;
+
+        while(go.transform.childCount > 0) {
+            Transform child = go.transform.GetChild(0);
+            child.SetParent(parent);
+            child.rotation = Quaternion.identity;
+        }
+
+        GameObject.DestroyImmediate(go);
+    }
+
+    [MenuItem("GameObject/Unpack Coins", true)]
+    public static bool CoinCheck() {
+        return Selection.activeGameObject != null && Selection.activeGameObject.transform.childCount > 0 && Selection.activeGameObject.transform.parent != null;
+    }
+}
